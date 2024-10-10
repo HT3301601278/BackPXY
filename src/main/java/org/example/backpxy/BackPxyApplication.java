ca2ae20cff933ca562d637ec49d34edc17770182
ÊÀ¼ä΢˹ÈË <3301601278@qq.com>
2024-10-10 17:48:00 +0800

    完善

---
+++ src/main/java/org/example/backpxy/BackPxyApplication.java
@@ -0,0 +1,13 @@
+package org.example.backpxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPxyApplication.class, args);
+    }
+
+}
